3f8d5ed4d440aa8d49b79415585ab1b510027419
YevhenSu <susidka.yevhen@gmail.com>
2018-06-17 20:34:00 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+const shell = require('shelljs');
+var app = express();
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+res.sendFile( __dirname + "/" + "index.html" );
+})
+app.get('/process_get', function (req, res) {
+// Prepare output in JSON format
+response = {
+first_name:req.query.first_name,
+last_name:req.query.last_name
+};
+console.log(response);
+shell.exec('./shellScript/status.sh')
+res.end(JSON.stringify(response));
+})
+var server = app.listen(8081, function () {
+var host = server.address().address
+var port = server.address().port
+console.log("Example app listening at http://%s:%s", host, port)
+})
